8fc1ea45c2e09fba3d8fcda29f85c6ce589fb4bd
unknown <singh777komal@gmail.com>
2023-09-24 19:05:42 +0530

    tag

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,15 +1,22 @@
 var header=document.querySelector('#main-header');
 header.style.borderBottom='solid 3px #000';
 
-var items=document.getElementsByClassName('list-group-item');
+// var items=document.getElementsByClassName('list-group-item');
 // console.log(items[0]);
 
 // items[0].style.fontWeight='bold'
 // items[1].style.color='green'
 // items[2].textContent='Hello';
-items[3].style.backgroundColor='green'
+// items[3].style.backgroundColor='green'
 
 
-for(var i=0; i<items.length; i++){
-    items[i].style.fontWeight='bold'
+// for(var i=0; i<items.length; i++){
+//     items[i].style.fontWeight='bold'
+// }
+
+
+var li=document.getElementsByTagName('li');
+console.log(li);
+for(var i=0; i<li.length; i++){
+    li[i].style.backgroundColor='yellow';
 }

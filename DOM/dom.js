a20418d12f411fdc1fda93c969c9040da6c50106
unknown <singh777komal@gmail.com>
2023-09-24 18:57:28 +0530

    change

--- DOM/dom.js
+++ DOM/dom.js
@@ -2,9 +2,14 @@ var header=document.querySelector('#main-header');
 header.style.borderBottom='solid 3px #000';
 
 var items=document.getElementsByClassName('list-group-item');
-console.log(items[0]);
+// console.log(items[0]);
 
-items[0].style.fontWeight='bold'
-items[1].style.color='green'
-items[2].textContent='Hello';
+// items[0].style.fontWeight='bold'
+// items[1].style.color='green'
+// items[2].textContent='Hello';
 items[3].style.backgroundColor='green'
+
+
+for(var i=0; i<items.length; i++){
+    items[i].style.fontWeight='bold'
+}

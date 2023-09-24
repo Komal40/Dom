7301b60ee7e20387ab232d5b456069edff0909c0
unknown <singh777komal@gmail.com>
2023-09-24 19:55:21 +0530

    query-selector

--- DOM/dom.js
+++ DOM/dom.js
@@ -15,8 +15,34 @@ header.style.borderBottom='solid 3px #000';
 // }
 
 
-var li=document.getElementsByTagName('li');
-console.log(li);
-for(var i=0; i<li.length; i++){
-    li[i].style.backgroundColor='yellow';
+// var li=document.getElementsByTagName('li');
+// console.log(li);
+// for(var i=0; i<li.length; i++){
+//     li[i].style.backgroundColor='yellow';
+// }
+
+
+
+//by queryselector
+// var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor='green';
+
+// var thirdItem=document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.color='white';
+
+
+
+
+
+
+
+//by queryselectorall
+var secondItem=document.querySelectorAll('li');
+secondItem[1].style.color='green'
+
+var odd=document.querySelectorAll('li:nth-child(odd');
+for(var i=0;i<odd.length; i++){
+    odd[i].style.backgroundColor='green'
 }
+
+

ef5ec2985c70d0a47efcc672cdf3769345974f12
unknown <singh777komal@gmail.com>
2023-09-24 18:49:05 +0530

    dom

---
+++ Asg1- static design/index.js
@@ -0,0 +1,48 @@
+// function submitInfo(event) {
+//     event.preventDefault();
+//     console.log(document.getElementById('fname').value);
+//     console.log(document.getElementById('email').value);
+//     console.log(document.getElementById('tel').value);
+//     console.log(document.getElementById('date').value);
+//     console.log(document.getElementById('time').value);
+
+// }
+
+
+// const ul=document.querySelector('.items');
+
+// ul.firstElementChild.textContent='Hello';
+// ul.firstElementChild.style.color='green';
+
+// ul.children[2].style.background='yellow';
+
+
+
+
+
+const form=document.querySelector('#my-form');
+const nameInp=document.querySelector('#fname');
+const email=document.querySelector('#email');
+const phn=document.querySelector('#tel');
+const date=document.querySelector('#date');
+const time=document.querySelector('#time');
+const msg=document.querySelector('.msg');
+const btn=document.querySelector('#btn');
+
+
+form.addEventListener('submit', onSubmit);
+// btn.addEventListener('mouseover', onSubmit);
+// btn.addEventListener('mouseout', onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+    if(nameInp.value=='' || email.value=='' || phn.value==''){
+        msg.innerHTML='<h4>Please Enter feilds...</h4>';
+        setTimeout(()=>msg.remove(), 3000);
+    }
+    else{
+        console.log(nameInp.value+", "+email.value+", "+phn.value+", "+date.value+", "+time.value);
+    }
+}
+
+
---
+++ DOM/dom.js
@@ -0,0 +1,10 @@
+var header=document.querySelector('#main-header');
+header.style.borderBottom='solid 3px #000';
+
+var items=document.getElementsByClassName('list-group-item');
+console.log(items[0]);
+
+items[0].style.fontWeight='bold'
+items[1].style.color='green'
+items[2].textContent='Hello';
+items[3].style.backgroundColor='green'

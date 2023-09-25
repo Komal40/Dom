2061ce1b9bbf5580f82c54fbaf70fc2cb1be7218
unknown <singh777komal@gmail.com>
2023-09-25 12:40:44 +0530

    manipulateDom

--- DOM/dom.js
+++ DOM/dom.js
@@ -37,12 +37,73 @@ header.style.borderBottom='solid 3px #000';
 
 
 //by queryselectorall
-var secondItem=document.querySelectorAll('li');
-secondItem[1].style.color='green'
+// var secondItem=document.querySelectorAll('li');
+// secondItem[1].style.color='green'
 
-var odd=document.querySelectorAll('li:nth-child(odd');
-for(var i=0;i<odd.length; i++){
-    odd[i].style.backgroundColor='green'
-}
+// var odd=document.querySelectorAll('li:nth-child(odd');
+// for(var i=0;i<odd.length; i++){
+//     odd[i].style.backgroundColor='green'
+// }
 
 
+
+
+//traversing dom
+var itemList=document.querySelector('#items')
+// console.log(itemList.parentNode.parentNode.parentNode)
+// itemList.parentNode.style.backgroundColor='yellow'
+
+// console.log(itemList.parentElement)
+// itemList.parentElement.style.backgroundColor='red'
+
+// console.log(itemList.childNodes)
+// console.log(itemList.children)
+// itemList.children[1].style.backgroundColor='yellow'
+
+// console.log(itemList.firstChild)
+// console.log(itemList.firstElementChild)
+// itemList.firstElementChild.textContent='Hello'
+
+
+
+// console.log(itemList.lastChild)
+// console.log(itemList.lastElementChild)
+// itemList.lastElementChild.textContent='Bye'
+
+// console.log(itemList.nextSibling)
+// console.log(itemList.nextElementSibling)
+
+// console.log(itemList.previousElementSibling)
+
+
+
+//ADD HELLO WORD BEFORE ITEM LISTER
+var newDiv=document.createElement('div')
+// newDiv.className='Hello'
+// newDiv.id='hii'
+
+// newDiv.setAttribute('title', 'hello')
+var newDivText=document.createTextNode('HELLO')
+newDiv.appendChild(newDivText)
+
+var container=document.querySelector('header .container')
+var h1=document.querySelector('header h1')
+
+// console.log(newDiv)
+container.insertBefore(newDiv, h1)
+
+
+
+
+//ADD HELLO WORD BEFORE ITEM 1
+var newli=document.createElement('li')
+newli.className='list-group-item'
+var newliText=document.createTextNode('Hello')
+newli.appendChild(newliText)
+
+var list=document.querySelector('ul.list-group')
+var li=document.querySelector('ul.list-group li')
+list.insertBefore(newliText, li)
+
+
+console.log(newli)
